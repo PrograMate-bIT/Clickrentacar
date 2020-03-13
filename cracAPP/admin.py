e14b1feb92cd9a60816ba10f677a756d81051a13
Matias Mutter <matimutter@gmail.com>
2020-03-13 16:32:27 -0300

    update makemigrations
    http://127.0.0.1:8000/admin/ -> ya se puede ver las tablas <usuario: admin> <contraseña: admin>

--- cracAPP/admin.py
+++ cracAPP/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(ciudad.Ciudad)
+admin.site.register(cuenta.Cuenta)
+admin.site.register(cuentaAdministrador.Administrador)
+admin.site.register(cuentaUsuario.Usuario)
+admin.site.register(denegacion.Denegacion)
+admin.site.register(licencia.Licencia)
+admin.site.register(pais.Pais)
+admin.site.register(perfilAlquila.PerfilAlquila)
+admin.site.register(perfilRenta.PerfilRenta)
+admin.site.register(registroAlquiler.RegistroAlquiler)
+admin.site.register(solicitudAlquiler.SolicitudAlquiler)
+admin.site.register(solicitudRegistro.SolicitudRegistro)
+admin.site.register(vehiculo.Vehiculo)
+admin.site.register(vehiculoPapeles.VehiculoPapeles)
