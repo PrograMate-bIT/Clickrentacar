1ae5a0199bc8d829acc8df6e345ea4daa471acea
Matias Mutter <matimutter@gmail.com>
2020-03-13 15:50:58 -0300

    models funcionando
    todo ok

--- cracAPP/models/__init__.py
+++ cracAPP/models/__init__.py
@@ -1,6 +1,2 @@
-# from . import ciudad, cuenta, cuentaAdministrador, cuentaUsuario, denegacion, licencia, pais, perfilAlquila, \
-#    perfilRenta, registroAlquiler, solicitudAlquiler, solicitudRegistro, vehiculo, vehiculoPapeles
-
-from . import pais, ciudad, vehiculo, vehiculoPapeles, cuenta, cuentaAdministrador, cuentaUsuario, perfilAlquila, \
-    perfilRenta, \
-    solicitudAlquiler, solicitudRegistro
+from . import ciudad, cuenta, cuentaUsuario, cuentaAdministrador, denegacion, licencia, pais, perfilAlquila, \
+    perfilRenta, registroAlquiler, solicitudAlquiler, solicitudRegistro, vehiculo, vehiculoPapeles
--- cracAPP/models/denegacion.py
+++ cracAPP/models/denegacion.py
@@ -6,9 +6,9 @@ from . import cuentaUsuario, cuentaAdministrador
 class Denegacion(models.Model):
     id = models.IntegerField(primary_key=True)
     numeroSolicitud = models.IntegerField()
-    ciSolicitante = models.ForeignKey(cuentaUsuario.Usuario, on_delete=models.CASCADE())
-    pais = models.ForeignKey(cuentaUsuario.pais, on_delete=models.CASCADE())
-    ciudad = models.ForeignKey(cuentaUsuario.ciudad, on_delete=models.CASCADE)
+    ciSolicitante = models.ForeignKey(cuentaUsuario.Usuario, on_delete=models.CASCADE)
+    pais = models.ForeignKey(cuentaUsuario.pais.Pais, on_delete=models.CASCADE)
+    ciudad = models.ForeignKey(cuentaUsuario.ciudad.Ciudad, on_delete=models.CASCADE)
     tipoDnegacion = models.TextChoices('Alquilar', 'Rentar')
     comentarioDenegacion = models.CharField(max_length=100)
-    administradorDenegador = models.ForeignKey(cuentaAdministrador.Administrador, on_delete=models.CASCADE())
+    administradorDenegador = models.ForeignKey(cuentaAdministrador.Administrador, on_delete=models.CASCADE)
