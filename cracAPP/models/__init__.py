8306e17693c49638c068a36ea364a3ac6c022db8
Matias Mutter <matimutter@gmail.com>
2020-03-13 14:53:47 -0300

    reformat code

--- cracAPP/models/__init__.py
+++ cracAPP/models/__init__.py
@@ -1,5 +1,6 @@
-#from . import ciudad, cuenta, cuentaAdministrador, cuentaUsuario, denegacion, licencia, pais, perfilAlquila, \
+# from . import ciudad, cuenta, cuentaAdministrador, cuentaUsuario, denegacion, licencia, pais, perfilAlquila, \
 #    perfilRenta, registroAlquiler, solicitudAlquiler, solicitudRegistro, vehiculo, vehiculoPapeles
 
-from . import pais, ciudad, vehiculo, vehiculoPapeles, cuenta, cuentaAdministrador, cuentaUsuario, perfilAlquila, perfilRenta, \
+from . import pais, ciudad, vehiculo, vehiculoPapeles, cuenta, cuentaAdministrador, cuentaUsuario, perfilAlquila, \
+    perfilRenta, \
     solicitudAlquiler, solicitudRegistro
