599a382f15948d4edcf0041dcf2224690a4fd23d
Matias Mutter <matimutter@gmail.com>
2020-03-15 14:35:19 -0300

    Update vehiculo.py

--- cracAPP/models/vehiculo.py
+++ cracAPP/models/vehiculo.py
@@ -2,6 +2,23 @@ from django.db import models
 
 
 class Vehiculo(models.Model):
+    AUTO = 'auto'
+    CAMIONETA = 'camioneta'
+    CAMION = 'camion'
+    MOTO = 'moto'
+
+    CATEGORIA_OPCIONES = [
+        (AUTO, 'Auto'),
+        (CAMIONETA, 'Camioneta'),
+        (CAMION, 'Camion'),
+        (MOTO, 'Moto'),
+    ]
+
     id = models.IntegerField(primary_key=True)
     marca = models.CharField(default="", max_length=15)
     anio = models.IntegerField(default=1900, max_length=4)
+    categoria = models.CharField(
+        max_length=15,
+        choices=CATEGORIA_OPCIONES,
+        default=AUTO,
+    )
