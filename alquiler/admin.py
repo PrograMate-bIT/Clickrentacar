23a6afcc183afb47bd6731b5a194c62e9bd6bcda
JavierGianneo <38515384+JavierGianneo@users.noreply.github.com>
2020-03-25 23:33:29 -0300

    Ahora funciona (#14)

--- alquiler/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from alquiler.models import SolicitudAlquiler
-from alquiler.models import RegistroAlquiler
-
-admin.site.register(SolicitudAlquiler)
-admin.site.register(RegistroAlquiler)
--- alquiler/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AlquilerConfig(AppConfig):
-    name = 'alquiler'
