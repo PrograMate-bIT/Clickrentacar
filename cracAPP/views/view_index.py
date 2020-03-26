f533c412d8249de77ec1c8eecae0c800f7867bcc
Matias Mutter <matimutter@gmail.com>
2020-03-25 23:22:34 -0300

    update views TODO FUNCIONANDO

--- cracAPP/views/view_index.py
+++ cracAPP/views/view_index.py
@@ -4,32 +4,26 @@ from usuario.models import Ciudad
 
 def index(request):
     # TODO ESTO ES UNA PRUEBA
-    texto = "texto de prueba"
-    dato = 123
     titulo = "Click Rent a Carrrr"
+    dato = 1024+1024
+    texto = "texto de prueba"
+    atributo1 = ''
+    atributo2 = ''
+    atributo3 = ''
 
     try:
-        variable = Ciudad(ciudad='Canelones', pais='Uruguay')
+        nueva_ciudad = Ciudad(ciudad='Paysandú', pais='Uruguay')
+        texto = "Intentando guardar ciudades en la DB"
+        atributo1 = "String de la clase: " + str(nueva_ciudad)
+        atributo2 = "Pais: " + nueva_ciudad.pais
+        atributo3 = "Ciudad: " + nueva_ciudad.ciudad
         try:
-            variable.save()  # Guarda ciudad en la DB
+            nueva_ciudad.save()  # Guarda ciudad en la DB
+            texto = "Se guardaró [" + str(nueva_ciudad) + "] en la tabla Ciudades"
         except:
-            print("--------------------------------------------")
-            print("ya existe " + str(variable)) + "en la db."
-            print("--------------------------------------------")
-    except Exception as e:
-        print(str(e))
-        variable = "variable error, el model no anda"
-    atributo1 = str(variable)
-
-    try:
-        atributo2 = variable.ciudad
-    except:
-        atributo2 = "atributo2 no anda"
-    try:
-        atributo3 = variable.pais
-    except:
-        atributo3 = "atributo3 no anda"
-
+            texto = "No se guradó [" + str(nueva_ciudad) + "], ya existe en la db."
+    except Exception as error:
+        texto = "ERROR EN INDEX.PY" + str(error)
 
     return render(request, 'cracAPP/index.html', {'title': titulo,
                                                   'texto': texto,
