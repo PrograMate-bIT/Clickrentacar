c0df0e262c958638c58a757542919fb15f2a390a
Lenovo <javier.gianneo@gmail.com>
2020-03-25 23:10:15 -0300

    Ahora funciona

--- cracAPP/forms/loginVista.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class Login(forms.Form):
-    nombre = forms.CharField(label="nombreUsuario", required=True)
-    contrasenia = forms.CharField(label="contrasenia", required=True)
