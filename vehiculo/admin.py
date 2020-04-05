99d5723277fc2d7e60010d959ed3348b479d151a
Matias Mutter <matimutter@gmail.com>
2020-04-05 16:05:33 -0300

    c

--- vehiculo/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from vehiculo.models import Vehiculo
-from vehiculo.models import VehiculoPapeles
-
-# Register your models here.
-admin.site.register(Vehiculo)
-admin.site.register(VehiculoPapeles)
