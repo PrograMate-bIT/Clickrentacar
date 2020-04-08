963f9f44edbf70163ecf5b92f13dd606b3a5ec82
Matias Mutter <matimutter@gmail.com>
2020-04-07 23:22:15 -0300

    coso nuevo

--- cracAPP/FrontViews/view_userDocument.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def user_document(request):
-    return render(request, 'cracAPP/userDocument.html')
---
+++ vehicles/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from vehicles.models import Vehicle
+
+
+class VehicleRegisterForm(forms.ModelForm):
+    class Meta:
+        model = Vehicle
+        fields = ['carRegistration', 'brand', 'carModel', 'year', 'seatsNumber']
+        widgets = {
+            'carRegistration': forms.TextInput(attrs={'class': 'form-control mt-3', 'placeholder': 'Nick', 'max_length': '15'}),
+            'brand': forms.TextInput(attrs={'class': 'form-control mt-3', 'placeholder': 'Nick', 'max_length': '15'}),
+            'carModel': forms.TextInput(attrs={'class': 'form-control mt-3', 'placeholder': 'Nick', 'max_length': '15'}),
+            'year':forms.NumberInput(attrs={'class': 'form-control mt-3', 'placeholder': 'Nick', 'max_length': '15'}),
+            'seatsNumber':forms.NumberInput(attrs={'class': 'form-control mt-3', 'placeholder': 'Nick', 'max_length': '15'})
+        }
+
+    def save_model(self, request, obj, form, change):
+        obj.added_by = request.user
+        super().save_model(request, obj, form, change)
