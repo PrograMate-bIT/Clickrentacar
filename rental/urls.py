f7b0d4c07b57c25a16049d54e4d38f7f2c93c7c3
Lenovo <javier.gianneo@gmail.com>
2020-04-15 00:45:05 -0300

    Merge branch 'master' into javier
    # Conflicts:
    #	.gitignore
    #	core/static/core/vendor/font-awesome/fonts/fontawesome-webfont.svg
    #	rental/urls.py
    #	rental/views.py
    #	vehicles/templates/vehicles/vechiclepublication_detail.html
    #	vehicles/templates/vehicles/vehicle_list.html
    #	vehicles/views.py

--- rental/urls.py
+++ rental/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from rental.views import CreateRentalView, MyCarRequestView
+from rental.views import CreateRentalView, MyCarRequestView, CreateConfirmedRentalView
 
 urlpatterns = [
     path('<int:pk>/', CreateRentalView.as_view(), name="new_rental_request"),
-    path('rental_request/',MyCarRequestView.as_view(), name="rental_request"),
+    path('<int:pk>/rental_request/', MyCarRequestView.as_view(), name="rental_request"),
+    path('<int:pk>/rental_aprobe/', CreateConfirmedRentalView.as_view(), name="rental_aprobe")
 ]
--- vehicles/views.py
+++ vehicles/views.py
@@ -13,16 +13,21 @@ class VehicleRegisterView(CreateView):
     template_name = "vehicles/vehicleRegister.html"
 
     def get_success_url(self):
-        return reverse_lazy('my_vehicles') + '?requested'
+        return reverse_lazy('my_vehicles') + '?register'
 
     def get_form(self, form_class=None):
         form = super(VehicleRegisterView, self).get_form()
 
-        form.fields['carRegistration'].widget = forms.TextInput(attrs={'class': 'form-control mb-2', 'placeholder': 'Matricula'})
-        form.fields['brand'].widget = forms.TextInput(attrs={'class': 'form-control mb-2', 'placeholder': 'Marca'})
-        form.fields['carModel'].widget = forms.TextInput(attrs={'class': 'form-control mb-2', 'placeholder': 'Modelo'})
-        form.fields['year'].widget = forms.NumberInput(attrs={'class': 'form-control mb-2', 'placeholder': 'Año'})
-        form.fields['seatsNumber'].widget = forms.NumberInput(attrs={'class': 'form-control mb-2', 'placeholder': 'Numero de asientos'})
+        form.fields['carRegistration'].widget = forms.TextInput(
+            attrs={'class': 'form-control mb-2', 'placeholder': 'Matricula'})
+        form.fields['brand'].widget = forms.TextInput(
+            attrs={'class': 'form-control mb-2', 'placeholder': 'Marca'})
+        form.fields['carModel'].widget = forms.TextInput(
+            attrs={'class': 'form-control mb-2', 'placeholder': 'Modelo'})
+        form.fields['year'].widget = forms.NumberInput(
+            attrs={'class': 'form-control mb-2', 'placeholder': 'Año'})
+        form.fields['seatsNumber'].widget = forms.NumberInput(
+            attrs={'class': 'form-control mb-2', 'placeholder': 'Numero de asientos'})
         return form
 
     def form_valid(self, form):
@@ -86,7 +91,7 @@ class CreatePublicationView(CreateView):
 
     def form_valid(self, form):
         form.instance.publisher = self.request.user.profile
-        form.instance.vehicle = Vehicle.objects.get(id=self.kwargs.get('pk'))
+        form.instance.vehicle = Vehicle.objects.get(id=self.kwargs.get('veihcle_pk'))
         return super().form_valid(form)
 
 @method_decorator(login_required, name='dispatch')
