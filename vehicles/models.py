12faaabb49a4a709d06cda80b428bb1c6d52dae9
Lenovo <javier.gianneo@gmail.com>
2020-04-19 18:45:34 -0300

    base

--- vehicles/models.py
+++ vehicles/models.py
@@ -1,8 +1,5 @@
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 from registration.models import Profile, custom_upload_to
-from rental.models import Confirmed
 
 
 class Vehicle(models.Model):
@@ -13,6 +10,7 @@ class Vehicle(models.Model):
     carModel = models.CharField(max_length=15, blank=False)
     year = models.IntegerField(default=1900, blank=True)
     seatsNumber = models.IntegerField(2, blank=True)
+    published = models.BooleanField(default=True)
 
     class Meta:
         ordering = ['carRegistration']
